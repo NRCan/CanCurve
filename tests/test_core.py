23be09c65fbd87f015480fcc5858057e7e1f01dd
Seth Bryant <bryant.seth@gmail.com>
2024-04-16 20:50:57 -0600

    fix test data

--- tests/test_core.py
+++ tests/test_core.py
@@ -42,13 +42,20 @@ def proj_db_fp(tmp_path):
 # tests---------
 #===============================================================================
 
-def test_c00_setup_project( tmp_path):
+def test_c00_setup_project(tmp_path):
     from cancurve.core import c00_setup_project as func
-    func(out_dir=tmp_path, bldg_meta={'testFeat1':'v1', 'testFeat2':'v2'}, curve_name='testCurve')
+    func(out_dir=tmp_path, bldg_meta={'mf_height_m':1.8, 'mf_area_m2':232.0}, curve_name='c00')
     
     
 @pytest.mark.dev
 @pytest.mark.parametrize('testCase',['case1'], indirect=False)
 def test_c01_join_drf(ci_fp, proj_db_fp, tmp_path):
-    from cancurve.core import c01_join_drf as func
+    from cancurve.core import c01_join_drf as func   
+    
+
+    #get a copy of the project database
+    proj_db_fp = shutil.copy(
+        os.path.join(test_data_dir_master, 'c00_20240416.cancurve'), 
+        os.path.join(tmp_path, 'c00.cancurve'))
+    
     func(ci_fp, proj_db_fp, out_dir=tmp_path)
