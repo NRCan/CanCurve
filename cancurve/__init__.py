577c216223c67ace510c1d416adee534c56f1426
Seth Bryant <bryant.seth@gmail.com>
2024-05-01 11:31:22 -0600

    add dependency check

--- cancurve/__init__.py
+++ cancurve/__init__.py
@@ -1,6 +1,11 @@
-
+#===============================================================================
+# plugin metadata
+#===============================================================================
 __version__='0.0'
 
+#===============================================================================
+# plugin entry point
+#===============================================================================
 # noinspection PyPep8Naming
 def classFactory(iface):  # pylint: disable=invalid-name
     """Load CanCurve class from file CanCurve.
@@ -11,3 +16,22 @@ def classFactory(iface):  # pylint: disable=invalid-name
     #
     from .plugin import CanCurve
     return CanCurve(iface)
+
+#===============================================================================
+# dependency check
+#===============================================================================
+
+ 
+
+import importlib, warnings
+
+def check_package(package_name):
+    spec = importlib.util.find_spec(package_name)
+    if spec is not None:
+        print(f'module {package_name} is installed')
+    else:
+        warnings.warn(f'module \'{package_name}\' not installed')
+
+ 
+check_package('openpyxl')
+
