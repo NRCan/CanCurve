ec0c84114e87339bc86aa039d75a055748ba488f
Seth Bryant <bryant.seth@gmail.com>
2024-10-31 11:17:09 -0600

    fix 'storys'

--- cancurve/bldgs/parameters_ui.py
+++ cancurve/bldgs/parameters_ui.py
@@ -13,7 +13,7 @@ building_details_options_d = {
     #general
     'occupancyClassification': ['Residential', 'Commercial', 'Industrial', 'Other'],
     'subClassification': ['Single Family', 'Construction', 'Duplex', 'Entertainment and Recreation', 'Food/Drugs/Chemicals', 'Heavy', 'High rise apartment', 'High technology', 'Hospital', 'Light', 'Low rise apartment', 'Medical office', 'Metals/Minerals Processing', 'Mobile home', 'Multifamily', 'Other', 'Personal and repair', 'Recreational', 'Retail trade', 'Theatre', 'Townhouse', 'Wholesale trade'],
-    'storys': ['1', '2', 'Split', 'Multi-story'],
+    'storys': ['1', '2', 'Split', 'Multi-storey'],
     
     'heatingType': ['Baseboard-electric', 'Baseboard-hot Water', 'Forced Air - Electric', 'Forced Air - Gas', 'Forced Air - Unknown', 'Heatpump', 'Mixed', 'Solar', 'Oil', 'Woodstove', 'Other'],
     'coolingType': ['Central air', 'Heat pump'],
